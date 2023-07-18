a4eaf76bfcfd5064b25e6195de8bc0435693ebf1
soohykim <24tngus@naver.com>
2023-07-18 11:54:19 +0900

    java ~ 5.문자열

---
+++ baekjoon/baek12.java
@@ -0,0 +1,22 @@
+// 문자열 반복
+
+import java.util.*;
+
+public class baek12 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+
+        int T = in.nextInt();
+        for (int n = 0; n < T; n++) {
+            int R = in.nextInt();
+            String S = in.next();
+            for (int i = 0; i < S.length(); i++) {
+                for (int j = 0; j < R; j++) {
+                    System.out.print(S.charAt(i));
+                }
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}

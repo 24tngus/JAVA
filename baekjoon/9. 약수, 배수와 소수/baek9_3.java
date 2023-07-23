d9286962ebc224962ac11772b9deae63a4e51ca1
soohykim <24tngus@naver.com>
2023-07-23 20:50:21 +0900

    9

--- baekjoon/9. 약수, 배수와 소수/baek9_3.java
+++ baekjoon/9. 약수, 배수와 소수/baek9_3.java
@@ -6,20 +6,33 @@ public class baek9_3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int N = in.nextInt();
-        int K = in.nextInt();
-        int[] factor = new int[N];
-        int j = 0;
+        while(true) {
+            int N = in.nextInt();
+            if (N == -1)
+                break;
+            int[] factor = new int[N];
+            int j = 0;
+            int sum = 0;
 
-        for (int i = 1; i <= N; i++) {
-            if (N % i == 0)
-                factor[j++] = i;
+            for (int i = 1; i <= N; i++) {
+                if (N % i == 0 && N != i) {
+                    factor[j++] = i;
+                    sum += i;
+                }
+            }
+            if (sum == N) {
+                System.out.print(N + " = 1");
+                for(int i = 2; i <= N; i++) {
+                    if (factor[i - 1] != 0) {
+                        System.out.print(" + " + factor[i - 1]);
+                    }
+                }
+                System.out.println();
+            }
+            else {
+                System.out.println(N + " is NOT perfect.");
+            }
         }
-        if (K > j)
-            System.out.println(0);
-        else
-            System.out.println(factor[K - 1]);
-
         in.close();
     }
 }

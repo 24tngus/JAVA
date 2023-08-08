8dcf856bc79ca12f24490611135ed274e94011ea
soohykim <24tngus@naver.com>
2023-08-08 22:05:05 +0900

    14. 집합과 맵

--- baekjoon/14. 집합과 맵/baek14_3.java
+++ baekjoon/14. 집합과 맵/baek14_3.java
@@ -1,11 +1,28 @@
-// 
+// 회사에 있는 사람
 
+import java.io.*;
 import java.util.*;
 
 public class baek14_3 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        in.close();
+        int N = Integer.parseInt(br.readLine());
+        Map<String, String> map = new HashMap<>();
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String key = st.nextToken();
+            String value = st.nextToken();
+            if (map.containsKey(key))
+                map.remove(key);
+            else
+                map.put(key, value);
+        }
+        ArrayList<String> list = new ArrayList<String>(map.keySet());
+        Collections.sort(list, Collections.reverseOrder());
+        for (var value : list)
+            System.out.println(value + " ");
     }
 }

6b6008812c3612b5e329a47c6e8244111d18dd98
soohykim <24tngus@naver.com>
2023-08-08 22:28:56 +0900

    14. 집합과 맵

--- baekjoon/14. 집합과 맵/baek14_4.java
+++ baekjoon/14. 집합과 맵/baek14_4.java
@@ -1,11 +1,37 @@
-// 
+// 나는야 포켓몬 마스터 이다솜
 
 import java.util.*;
+import java.io.*;
 
 public class baek14_4 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        in.close();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Map<String, Integer> smap = new HashMap<>();
+        Map<Integer, String> imap = new HashMap<>();
+
+        for (int i = 1; i <= N; i++) {
+            String name = br.readLine();
+            smap.put(name, i);
+            imap.put(i, name);
+        }
+        for (int i = 1; i <= M; i++) {
+            String str = br.readLine();
+            if (isNum(str)) {
+                System.out.println(imap.get(Integer.parseInt(str)));
+            } else {
+                System.out.println(smap.get(str));
+            }
+        }
+    }
+    public static boolean isNum(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i)))
+                return false;
+        }
+        return true;
     }
 }

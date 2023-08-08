4b7bef5e53b6491cdba01b3204798050e04f6cc0
soohykim <24tngus@naver.com>
2023-08-08 21:41:10 +0900

    14. 집합과 맵

--- baekjoon/14. 집합과 맵/baek14_1.java
+++ baekjoon/14. 집합과 맵/baek14_1.java
@@ -1,12 +1,42 @@
-// 
+// 숫자 카드
 
+import java.io.*;
 import java.util.*;
 
 public class baek14_1  {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = in.nextInt();
-        in.close();
+        int count_N = Integer.parseInt(br.readLine());
+        int[] N = new int[count_N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < count_N; i++) {
+            N[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(N);
+
+        int count_M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0;  i < count_M; i++) {
+            int search = Integer.parseInt(st.nextToken());
+            System.out.print(binary_search(N, count_N, search) + " ");
+        }
+    }
+    public static int binary_search(int[] N, int count, int search) {
+        int first = 0;
+        int last = count - 1;
+
+        int mid = 0;
+        while (first <= last) {
+            mid = (first + last) / 2;
+            if (N[mid] == search)
+                return (1);
+            if (N[mid] < search) {
+                first = mid + 1;
+            } else {
+                last = mid - 1;
+            }
+        }
+        return (0);
     }
 }
--- baekjoon/14. 집합과 맵/baek14_2.java
+++ baekjoon/14. 집합과 맵/baek14_2.java
@@ -1,11 +1,25 @@
-// 
+// 문자열 집합 
 
+import java.io.*;
 import java.util.*;
 
 public class baek14_2 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        in.close();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            map.put(br.readLine(), 0);
+        }
+        int count = 0;
+        for (int i = 0; i < M; i++) {
+            if (map.containsKey((br.readLine())))
+                count++;
+        }
+        System.out.println(count);
     }
 }

00e5012d84a06630bff6543a4d1fef26ea839762
soohykim <24tngus@naver.com>
2023-08-14 19:14:22 +0900

    16

--- baekjoon/16. 스택, 큐, 덱/baek16_2.java
+++ baekjoon/16. 스택, 큐, 덱/baek16_2.java
@@ -1,4 +1,4 @@
-//
+// 제로
 
 import java.util.*;
 import java.io.*;
@@ -6,6 +6,27 @@ import java.io.*;
 public class baek16_2 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<Integer>();
         
+        int K = Integer.parseInt(br.readLine());
+        int result = 0;
+        for (int i = 0; i < K; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0)
+            {
+                if (!stack.isEmpty())
+                    stack.pop();
+            }
+            else
+                stack.push(n);
+        }
+        while (!stack.isEmpty())
+        {
+            result += stack.peek();
+            stack.pop();
+        }
+        sb.append(result).append("\n");
+        System.out.println(sb);
     }
 }

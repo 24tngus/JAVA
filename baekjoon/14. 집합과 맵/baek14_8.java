cc54f6defad81990e581fc174d4223f3030e281f
soohykim <24tngus@naver.com>
2023-08-15 20:16:14 +0900

    14

--- baekjoon/14. 집합과 맵/baek14_8.java
+++ baekjoon/14. 집합과 맵/baek14_8.java
@@ -5,7 +5,16 @@ import java.util.*;
 public class baek14_8 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String s = in.next();
 
+        Set<String> set = new HashSet<String>();
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                set.add(s.substring(i, j));
+            }
+        }
+        System.out.println(set.size());
         in.close();
     }
 }

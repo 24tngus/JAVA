50146cc7e5686d6590bb2b0788b7bb4543a8786f
soohykim <24tngus@naver.com>
2023-08-18 21:23:39 +0900

    20. 심화2

---
+++ baekjoon/20. 심화2/baek20_3.java
@@ -0,0 +1,12 @@
+//
+
+import java.util.*;
+import java.io.*;
+
+public class baek20_3 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+    }
+}

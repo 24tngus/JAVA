1e1c638fba1408d5b7340c2e282c67d00ef1290b
soohykim <24tngus@naver.com>
2023-08-20 00:00:03 +0900

    14

--- baekjoon/14. 집합과 맵/baek14_7.java
+++ baekjoon/14. 집합과 맵/baek14_7.java
@@ -1,12 +1,26 @@
 // 대칭 차집합
 
 import java.util.*;
+import java.io.*;
 
 public class baek14_7
  {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        in.close();
+        HashMap<Integer, Integer> map_A = new HashMap<>();
+        HashMap<Integer, Integer> map_B = new HashMap<>();
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < A; i++) {
+            
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < B; i++) {
+            
+        }
     }
 }

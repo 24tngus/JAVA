12e7b1fd34ed029341bb80df58c9584e7d8c6bf7
soohykim <24tngus@naver.com>
2023-09-03 15:35:46 +0900

    cospro

--- cospro/cos2_1_1.java
+++ cospro/cos2_1_1.java
@@ -1,34 +1,34 @@
-// You may use import as below.
-import java.util.*;
+// // You may use import as below.
+// import java.util.*;
 
-class Solution {
-    public int[] solution(String[] shirtSize) {
+// class Solution {
+//     public int[] solution(String[] shirtSize) {
 
-        int[] answer = new int[6];
-        for (int i = 0; i < shirtSize.length; i++) {
-            if (shirtSize[i].equals("XS"))
-                answer[0] += 1;
-            else if (shirtSize[i].equals("S"))
-                answer[1] += 1;
-            else if (shirtSize[i].equals("M"))
-                answer[2] += 1;
-            else if (shirtSize[i].equals("L"))
-                answer[3] += 1;
-            else if (shirtSize[i].equals("XL"))
-                answer[4] += 1;
-            else if (shirtSize[i].equals("XXL"))
-                answer[5] += 1;
-        }
-        return answer;
-    }
+//         int[] answer = new int[6];
+//         for (int i = 0; i < shirtSize.length; i++) {
+//             if (shirtSize[i].equals("XS"))
+//                 answer[0] += 1;
+//             else if (shirtSize[i].equals("S"))
+//                 answer[1] += 1;
+//             else if (shirtSize[i].equals("M"))
+//                 answer[2] += 1;
+//             else if (shirtSize[i].equals("L"))
+//                 answer[3] += 1;
+//             else if (shirtSize[i].equals("XL"))
+//                 answer[4] += 1;
+//             else if (shirtSize[i].equals("XXL"))
+//                 answer[5] += 1;
+//         }
+//         return answer;
+//     }
     
-    // The following is main method to output testcase.
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        String[] shirtSize = {"XS", "S", "L", "L", "XL", "S"};
-        int[] ret = sol.solution(shirtSize);
+//     // The following is main method to output testcase.
+//     public static void main(String[] args) {
+//         Solution sol = new Solution();
+//         String[] shirtSize = {"XS", "S", "L", "L", "XL", "S"};
+//         int[] ret = sol.solution(shirtSize);
  
-        // Press Run button to receive output.  
-        System.out.println("Solution: return value of the method is " + Arrays.toString(ret) + " .");
-    }
-}
+//         // Press Run button to receive output.  
+//         System.out.println("Solution: return value of the method is " + Arrays.toString(ret) + " .");
+//     }
+// }

087b67ad4372996b8e04d9664903a824bd36b590
soohykim <24tngus@naver.com>
2023-09-05 16:31:24 +0900

    codingtest nadongbin

---
+++ codingtest_na/na1_2.java
@@ -0,0 +1,11 @@
+//
+
+import java.util.*;
+import java.io.*;
+
+public class na1_2 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}

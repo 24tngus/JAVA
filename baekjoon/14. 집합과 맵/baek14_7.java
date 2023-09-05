fa67f9ef010cb6bc91286394d96d666ab0f5cbd9
soohykim <24tngus@naver.com>
2023-09-05 13:21:05 +0900

    baek 14. 집합과 맵

--- baekjoon/14. 집합과 맵/baek14_7.java
+++ baekjoon/14. 집합과 맵/baek14_7.java
@@ -3,24 +3,37 @@
 import java.util.*;
 import java.io.*;
 
-public class baek14_7
- {
+public class baek14_7 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        HashMap<Integer, Integer> map_A = new HashMap<>();
-        HashMap<Integer, Integer> map_B = new HashMap<>();
+        Set<Integer> set_A = new HashSet<>();
+        Set<Integer> set_B = new HashSet<>();
         
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < A; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            set_A.add(n);
         }
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < B; i++) {
-            
+            int n = Integer.parseInt(st.nextToken());
+            set_B.add(n);
         }
+
+        int result = 0;
+        for (int num : set_A) {
+            if (!set_B.contains(num))
+                result++;
+        }
+        for (int num : set_B) {
+            if (!set_A.contains(num))
+                result++;
+        }
+        System.out.println(result);
     }
 }
--- baekjoon/14. 집합과 맵/baek14_8.java
+++ baekjoon/14. 집합과 맵/baek14_8.java
@@ -1,21 +1,21 @@
-// 
+// 서로 다른 부분 문자열의 개수
 
 import java.util.*;
+import java.io.*;
 
 public class baek14_8 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s = in.next();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Set<String> set = new HashSet<String>();
+
+        String s = br.readLine();
 
         for (int i = 0; i < s.length(); i++) {
             for (int j = i + 1; j <= s.length(); j++) {
                 set.add(s.substring(i, j));
             }
         }
-        
         System.out.println(set.size());
-        in.close();
     }
 }

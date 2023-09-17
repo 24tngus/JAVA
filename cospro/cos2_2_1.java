4593f86c0854bc7bf83963a71dc160ebdf127129
soohykim <24tngus@naver.com>
2023-09-17 23:53:54 +0900

    g

--- cospro/cos2_2_1.java
+++ cospro/cos2_2_1.java
@@ -1,6 +1,11 @@
 import java.io.*;
+import java.util.*;
 
 public class cos2_2_1 {
-    System.out.printf();
+    public static void main(String args[]) {
+        System.out.println();
+
+        d
+    }
 
 }
